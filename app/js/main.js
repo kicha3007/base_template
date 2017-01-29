0c040c08660db969721090a14cc6b77827526438
Sergey Kichigin <kicha3007@gmail.com>
2017-01-29 11:14:14 +0500

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".slider-big").owlCarousel({
+        "items": 1,
+        "loop": true,
+        "dots": false,
+        "autoplay": true,
+        "autoplayTimeout": true,
+        "autoplayHoverPause": true,
+        "nav": false,
+    });
+});
